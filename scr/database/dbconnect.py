4bdbd8abf30c4e422dae77c6ff26c66682ec9bf5
Gustavo Briceño <gustav.dev.code@gmail.com>
2023-12-11 22:45:21 -0500

    Change of data from database to deployed database

--- scr/database/dbconnect.py
+++ scr/database/dbconnect.py
@@ -3,10 +3,18 @@ import mysql.connector
 def get_connection():
     try:
         return mysql.connector.connect(
-            host = "localhost",
-            user = "root",
-            password = "Kh1ng*7490@1963",
-            database = "attention_client"
+            host = "bzsehfulsyzmf1e718hl-mysql.services.clever-cloud.com",
+            user = "uhuqob7zeqs14vje",
+            password = "jw9z8U4fYIvImMscqqbG",
+            database = "bzsehfulsyzmf1e718hl",
+            port = 3306
         )
+        # return mysql.connector.connect(
+        #     host = "localhost",
+        #     user = "root",
+        #     password = "Kh1ng*7490@1963",
+        #     database = "attention_client",
+        #     port = 3306
+        # )
     except mysql.connector.errors as err:
         raise err
